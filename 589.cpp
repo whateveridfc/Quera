b8f09b975f1144ec0641e3071009cb1cfc8bdde1
whateveridfc <92092493+whateveridfc@users.noreply.github.com>
2021-12-30 18:47:26 +0330

    Add files via upload
    فاکتوریل

---
+++ 589.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+int main()
+{
+    int i , fact = 1, n;
+    cin >> n ;
+    for (i = 1; i <= n; i++) {
+        fact *= i;
+    }
+    cout << fact;
+    return 0;
+}
+
+
